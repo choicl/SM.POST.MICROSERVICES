4830eac9da4fe4ca64b37f7cb0d4fb5b9ae9fd41
Mariia Vasylchenko <mariia.vasylchenko@nice.com>
2023-07-16 21:41:25 +0300

    Initial commit

---
+++ SM-Post/Post.Common/Events/MessageUpdatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using CQRS.Core.Events;
+
+namespace Post.Common.Events
+{
+    public class MessageUpdatedEvent : BaseEvent
+    {
+        public MessageUpdatedEvent() : base(nameof(PostCreatedEvent))
+        {
+        }
+
+        public string Message { get; set; }
+    }
+}
