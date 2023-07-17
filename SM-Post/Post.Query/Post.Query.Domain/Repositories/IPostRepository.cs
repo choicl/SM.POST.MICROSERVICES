140162175c8edee68e4b6f73941c495685373d86
Mariia Vasylchenko <mariia.vasylchenko@nice.com>
2023-07-17 16:16:35 +0300

    Added domain layer: post and comment entities, repositories

---
+++ SM-Post/Post.Query/Post.Query.Domain/Repositories/IPostRepository.cs
@@ -0,0 +1,23 @@
+﻿using Post.Query.Domain.Entities;
+
+namespace Post.Query.Domain.Repositories
+{
+    public interface IPostRepository
+    {
+        Task CreateAsync(PostEntity post);
+
+        Task UpdateAsync(PostEntity post);
+
+        Task DeleteAsync(Guid postId);
+
+        Task<PostEntity> GetByIdAsync(Guid postId);
+
+        Task<List<PostEntity>> GetAllAsync();
+
+        Task<List<PostEntity>> GetAllByAuthorAsync(string author);
+
+        Task<List<PostEntity>> GetAllWithLikesAsync(int likes);
+
+        Task<List<PostEntity>> GetAllWithCommentsAsync();
+    }
+}
